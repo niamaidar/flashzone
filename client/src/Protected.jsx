a0f9e24debf554cf92f7eb7db41fa4b0fb0eef7c
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-22 19:09:11 +0100

    Add protected page

--- client/src/Protected.jsx
+++ client/src/Protected.jsx
@@ -1,9 +1,16 @@
 import React,{useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
 import Header from './Header'
-function Protected(){
+function Protected(props){
+    let Cmp=props.Cmp
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(!localStorage.getItem('user-info')){
+            navigate('/register')}
+    },[])
     return(
         <div>
-         
+            <Cmp/>
         </div>
     )
 }
