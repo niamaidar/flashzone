8b93107a43c0bd0e9f48d0bc4f82dc44d943ac76
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-22 18:48:28 +0100

    Update-Login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,6 +1,15 @@
+import React,{useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+import Header from './Header'
 function Login(){
+    const history = useHistory();
+    useEffect(()=>{
+        if(localStorage.getItem('user-info')){
+            history.push('/add')}
+    },[])
     return(
         <div>
+            <Header/>
             <h1>Login page</h1>
         </div>
     )
--- client/src/UpdateProduct.jsx
+++ client/src/UpdateProduct.jsx
@@ -1,6 +1,8 @@
+import Header from './Header'
 function UpdateProduct(){
     return(
         <div>
+            <Header/>
             <h1>UpdateProduct page</h1>
         </div>
     )
