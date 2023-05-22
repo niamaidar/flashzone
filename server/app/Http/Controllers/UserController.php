f908a40ed5ff03fda35838eb0a8d5618523c8428
niamaidar <niamaidar@gmail.com>
2023-05-22 19:33:21 +0100

    msg3

---
+++ server/app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    function register(Request $req)
+    {
+        // $user=new User;
+        // $user->name= $req->input('name');
+        // $user->email= $req->input('email');
+        // $user->password= Hash::make($req->input('password'));
+        // $user->save();
+        // return $user;
+        $user = User::create([
+            "name"=>$req->name,
+            "email"=>$req->email,
+            "password"=>Hash::make($req->password)
+        ]);
+        return response()->json([
+            'status'=>'succes',
+            'user'=>$user
+        ]);
+
+    }
+}
