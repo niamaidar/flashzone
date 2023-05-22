e5d459a6d0ae53f951b5043ebd1617d20c842fb8
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-22 17:57:51 +0100

    CreateNav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './Header';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
+import Login from './Login';
+import { BrowserRouter } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='underline'>
-      Hello World
-        
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <h1 className='text-center'> Flash zone Project </h1>
+        <Router path="./login"> <Login/> </Router>
+        <Router path="./add"> <AddProduct/> </Router> 
+        <Router path="./update"> <UpdateProduct/> </Router> 
+        <Router path="./register"> <Register/> </Router> 
+ 
+      </BrowserRouter>       
     </div>
   )
 }
