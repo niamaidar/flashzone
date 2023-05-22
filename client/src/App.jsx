f20b0f835004d4a161adc63dc32f6286e8b563b1
niamaidar <niamaidar@gmail.com>
2023-05-22 18:30:34 +0100

    routes hiba naima

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Register from './Register';
 import AddProduct from './AddProduct';
 import UpdateProduct from './UpdateProduct';
 import Login from './Login';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter ,Route,Routes } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,11 +16,14 @@ function App() {
       <BrowserRouter>
         <Header/>
         <h1 className='text-center'> Flash zone Project </h1>
-        <Router path="./login"> <Login/> </Router>
-        <Router path="./add"> <AddProduct/> </Router> 
-        <Router path="./update"> <UpdateProduct/> </Router> 
-        <Router path="./register"> <Register/> </Router> 
- 
+        <Routes>
+
+        <Route path="/" element={<Login/>}>  </Route>
+        <Route path="login" element={<Login/>}>  </Route>
+        <Route path="add" element={<AddProduct/>}>  </Route>
+        <Route path="update" element={<UpdateProduct/>}>  </Route>
+        <Route path="register" element={<Register/>}>  </Route>
+        </Routes>
       </BrowserRouter>       
     </div>
   )
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
 function Register(){
+
+    const [name , setName]=useState("")
+    const [password , setPassword]=useState("")
+    const [email , setEmail]=useState("")
+
+    async function signUp()
+    {
+        let item={name,password,email}  
+        console.warn(item)
+
+       let result=await fetch("https://localhst:8000/api/register",{ 
+       method:"POST",
+       body:JSON.stringify(item),
+       headers: {
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+    
+                }
+            })
+   
+   result=await result.json()
+   console.warn("result",result);
+
+        }
     return(
         <div>
             <h1>Register page</h1>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" />
         </div>
     )
 }
-export default Register;
+export default Register
