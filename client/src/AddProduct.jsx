a962ab60ed148a23a0cc10be11868566a68a99c7
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-25 22:46:23 +0100

    reglé

--- client/src/AddProduct.jsx
+++ client/src/AddProduct.jsx
@@ -1,36 +1,70 @@
+import axios from "axios";
 import Header from "./Header";
-import React, { useState } from 'react';function AddProduct(){
-    const [name,setName] = useState('');
-    const [file,setFile] = useState('');
-    const [description,setDescription] = useState('');
-    const [price,setPrice] = useState('');
-    async function addProduct(){
-        console.warn(name,file_path,price,description)
-        const formData= new FormData();
-        formData.append('file_path',file_path);
-        formData.append('price',price);
-        formData.append('description',description);
-        formData.append('name',name);
-        let  result = await fetch("http://localhost:8000/api/addproduct",{
-            method:'POST',
-            body: formData
-        });
-    }   alert('Data has been saved');
-    return(
-        <div>
-            <div className="col-sm-6 offset-sm-3">
-                <br/>
-                <input type="text" className="form-control" placeholder="name" onchange={(e)=>setName(e.target.value)} />
-                <br/>
-                <input type="text" className="form-control" placeholder="file" onchange={(e)=>setFile(e.target.value)} />
-                <br/>
-                <input type="text" className="form-control" placeholder="description" onchange={(e)=>setDescription(e.target.value)} />
-                <br/>
-                <input type="text" className="form-control" placeholder="price" onchange={(e)=>setPrice(e.target.value)} />
-                <br/>
-                <button  onclick={addProduct} className="btn btn-primary">Add product</button>
-            </div>
-        </div>
-    )
+import React, { useState } from "react";
+function AddProduct() {
+  const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  async function addProduct() {
+    // console.error(name, file, price, description);
+    // const formData = new FormData();
+    // formData.append("file_path", file);
+    // formData.append("price", price);
+    // formData.append("description", description);
+    // formData.append("name", name);
+    console.log({
+      name: name,
+      file: file,
+      price: price,
+      description: description,
+    });
+    const response = axios.post("http://localhost:8000/api/addproduct", {
+      name: name,
+      file_path: file,
+      price: price,
+      description: description,
+    });
+    console.log(response);
+    alert("Data has been saved");
+  }
+  return (
+    <div>
+      <div className="col-sm-6 offset-sm-3">
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="file"
+          onChange={(e) => setFile(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <br />
+        <button onClick={addProduct} className="btn btn-primary">
+          Add product
+        </button>
+      </div>
+    </div>
+  );
 }
 export default AddProduct;
