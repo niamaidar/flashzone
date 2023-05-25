ed0361a0ef3a0cadb19c9797c895612938d41281
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-25 19:41:15 +0100

    functionLogin

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -28,4 +28,13 @@ class UserController extends Controller
         ]);
 
     }
+    function login(Request $req)
+    {
+        $user=User::where('email',$req->email)->first();
+        if($user || !Hash::check($req->password,$user->password))
+        {
+            return ["error"=>"Email or password is not match"];
+        }
+        return $user;
+    }
 }
