31e03ef8bc810d1db278c16c586e2956dc4117aa
hiba-Ghl <115666230+hiba-Ghl@users.noreply.github.com>
2023-05-27 18:49:54 +0100

    up

--- client/src/UpdateProduct.jsx
+++ client/src/UpdateProduct.jsx
@@ -1,10 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function UpdateProduct() {
+  const [name, setName] = useState("");
+  const [file_path, setFile] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity,setQuantity] = useState("");
+  const [marque,setMarque] = useState("");
+  const [category,setCategory] = useState("");
   const { id } = useParams();
   const [data, setData] = useState('');
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -42,7 +49,7 @@ function UpdateProduct() {
     });
     navigate("/list");
     console.log(response);
-    alert("Data has been saved");
+    alert("Data has been updated");
   }
 
   return (
@@ -51,14 +58,21 @@ function UpdateProduct() {
         <div className="col-sm-6 offset-sm-3">
             <form>
             <fieldset>
-                <input type='text' defaultValue={data.name} className="form-control" placeholder='name'/> <br/> <br/>
-                <input type='text' defaultValue={data.price} className="form-control" placeholder='price'/> <br/> <br/>
-                <input type='file' defaultValue={data.file_path} className="form-control" placeholder='file path'/> <br/> <br/>
+                <input type='text' defaultValue={data.name} className="form-control" placeholder='name'
+                 onChange={(e) => setName(e.target.value)}/> <br/> <br/>
+                <input type='text' defaultValue={data.price} className="form-control" placeholder='price'
+                 onChange={(e) => setPrice(e.target.value)}/> <br/> <br/>
+                <input type='file' defaultValue={data.file_path} className="form-control" placeholder='file path'
+                 onChange={(e) => setFile(e.target.files[0])} /> <br/> <br/>
                 <img style={{width:100}} src={"http//:localhost:8000/"+data.file_path} /> <br/> <br/>
-                <input type='text' defaultValue={data.description} className="form-control" placeholder='description'/> <br/> <br/>
-                <input type='text' defaultValue={data.marque} className="form-control" placeholder='marque'/> <br/> <br/>
-                <input type='text' defaultValue={data.quantity} className="form-control" placeholder='quantity'/> <br/> <br/>
-                <input type='text' defaultValue={data.category} className="form-control" placeholder='category'/> <br/> <br/>
+                <input type='text' defaultValue={data.description} className="form-control" placeholder='description'
+                  onChange={(e) => setDescription(e.target.value)}/> <br/> <br/>
+                <input type='text' defaultValue={data.marque} className="form-control" placeholder='marque'
+                 onChange={(e) => setMarque(e.target.value)}/> <br/> <br/>
+                <input type='text' defaultValue={data.quantity} className="form-control" placeholder='quantity'
+                 onChange={(e) => setQuantity(e.target.value)}/> <br/> <br/>
+                <input type='text' defaultValue={data.category} className="form-control" placeholder='category'
+                 onChange={(e) => setCategory(e.target.value)}/> <br/> <br/>
                 <button onClick={updateProduct} className="btn btn-primary">Update product</button>
             </fieldset>
             </form>
